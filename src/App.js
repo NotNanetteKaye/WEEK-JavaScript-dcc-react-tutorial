e1cdf101dcd084f8554d3ab38a812b13dd3a6a88
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-09-22 14:51:45 -0500

    Completed AddEntry Folder/AddEntryForm Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries'
+import AddEntryForm from './Components/AddEntry/AddEntryForm'
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 176, date: '9-22-2022'}, {weight: 177, date: '9-23-2022'}])
+
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry];
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries = {entries} />
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
 
     </div>
   );
